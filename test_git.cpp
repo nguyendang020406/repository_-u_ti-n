f614e7521ee6b3c24d0142af4ada35f5e772ad56
Hoàng Minh <nguyendang020406@gmail.com>
2025-08-25 22:07:53 +0700

    Thêm fibonacci

--- test_git.cpp
+++ test_git.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+long long fibonacci(int n ){
+    int arr[n]={0};
+    arr[0]=0;
+    arr[1]=1;
+    for (int i=2;i< n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+}
 int main(){
     cout << "Hello world" << endl;
     cout << "mmbELS" << endl;
